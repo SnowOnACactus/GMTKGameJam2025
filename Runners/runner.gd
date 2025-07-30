9fb61bc7be4650f3aedd2015399ac45b79712e44
Ian Mortensen <Ian.Mortensen@nyu.edu>
2025-07-31 10:54:33 +1200

    -basic looping

--- Runners/runner.gd
+++ Runners/runner.gd
@@ -9,7 +9,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	#Run from right side to left
+	if global_position.x > get_viewport_rect().size.x + 50:
+		global_position.x -= get_viewport_rect().size.x + 90
+	
+	#Run from left to right - do we want this?
+	if global_position.x < -50:
+		global_position.x += get_viewport_rect().size.x + 90
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
