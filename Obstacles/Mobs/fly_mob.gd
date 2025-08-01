421b628c9205c39b478836e5e95b2b161ecf7588
Sean <s.gibbons745@gmail.com>
2025-08-02 11:18:22 +1200

    added collision to fly mob

--- Obstacles/Mobs/fly_mob.gd
+++ Obstacles/Mobs/fly_mob.gd
@@ -11,24 +11,15 @@ func _ready() -> void:
 	_sprite.play("fly")
 
 func _physics_process(delta: float) -> void:
-	super(delta)
 	if position.x > starting_position.x + 100.0:
 		_dir_right = false
-		_sprite.flip_h = true 
-		#comments
+		_sprite.flip_h = false
 	if position.x < starting_position.x - 100.0:
 		_dir_right = true
 		_sprite.flip_h = true
-	if position.y < starting_position.y - 50.0:
-		_dir_up = false
-	if position.y > starting_position.y + 50.0:
-		_dir_up = true
 	if _dir_right:
 		position.x += speed * delta
 	if !_dir_right:
 		position.x -= speed * delta
-	if _dir_up:
-		position.y -= speed * delta
-	if !_dir_up:
-		position.y += speed * delta
+	move_and_slide()
 	
